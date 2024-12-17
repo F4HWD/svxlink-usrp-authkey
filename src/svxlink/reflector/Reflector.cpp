92a1737292c6ac2aa0550ea8e4c79ac92214e41d
F4HWD Max <42820755+F4HWD@users.noreply.github.com>
2024-12-18 00:05:56 +0100

    Update Reflector.cpp
    Fix the bounce issue, again

--- src/svxlink/reflector/Reflector.cpp
+++ src/svxlink/reflector/Reflector.cpp
@@ -508,20 +508,20 @@ void Reflector::udpDatagramReceived(const IpAddress& addr, uint16_t port,
 
     // Check sequence number
   uint16_t udp_rx_seq_diff = header.sequenceNum() - client->nextUdpRxSeq();
-  /* if (udp_rx_seq_diff > 0x7fff) // Frame out of sequence (ignore)
+  if (udp_rx_seq_diff > 0x7fff) // Frame out of sequence (ignore)
   {
-    cout << client->callsign()
+    /* cout << client->callsign()
          << ": Dropping out of sequence frame with seq="
          << header.sequenceNum() << ". Expected seq="
-         << client->nextUdpRxSeq() << endl;
+         << client->nextUdpRxSeq() << endl; */
     return;
   }
    else if (udp_rx_seq_diff > 0) // Frame(s) lost
   {
-    cout << client->callsign()
+    /* cout << client->callsign()
          << ": UDP frame(s) lost. Expected seq=" << client->nextUdpRxSeq()
-         << ". Received seq=" << header.sequenceNum() << endl;
-  } */
+         << ". Received seq=" << header.sequenceNum() << endl; */
+  }
 
   client->udpMsgReceived(header);
 
